1a59ab815e7873fe2838838c27dfce2d9956297d
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-12 00:44:45 +0600

    Project Model

--- sequelize.js
+++ sequelize.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: 'university.db'
+  storage: 'university.sqlite'
 });
 
 export default sequelize;
