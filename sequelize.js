b1135686436f9b3953b1b28352aecd775bcfb2c6
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-14 16:10:57 +0600

    Postgres database connection

--- sequelize.js
+++ sequelize.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'university.sqlite'
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DAILECT
 });
 
 export default sequelize;
