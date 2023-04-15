2af1a8a42a9aa08f476302fc4e0e7b08c8ca9cd3
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-15 14:30:20 +0600

    Project schema

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ import Project from './src/models/Project';
 
 const PORT = process.env.PORT || 3000;
 
-// Project.sync({ force: true });
+Project.sync({ force: true });
 
 connect()
     .then(() => {
         app.listen(PORT, () => {
             connect();
-            sequelize.sync({ force: true });
+            // sequelize.sync({ force: true });
             console.log('Database connected');
             console.log(`Server is running on http://localhost:3000`);
         });
