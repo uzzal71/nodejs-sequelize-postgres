06aa8055b5db835408873f83ebdc5e0e4293661d
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-17 23:53:49 +0600

    User model email fields

--- src/models/User.js
+++ src/models/User.js
@@ -19,6 +19,10 @@ User.init({
     username: {
         type: DataTypes.STRING,
         allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
 }, {
     sequelize
