dac7fe5b82f8b9b09551de95f0c38168ffbeee0e
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-17 23:51:26 +0600

    User model firstName & lastName and sequelize initial

--- src/models/User.js
+++ src/models/User.js
@@ -4,10 +4,20 @@ import sequelize from "../config/sequelize";
 class User extends Model {};
 
 User.init({
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false
-    }
+    },
+}, {
+    sequelize
 });
 
 export default User;
