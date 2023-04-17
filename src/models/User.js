f68435f54238ec38d3222ae7f20ae0169827383c
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-17 23:48:03 +0600

    User model name create

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,13 @@
-import { Model, DataType } from "sequelize";
+import { Model, DataType, DataTypes } from "sequelize";
 import sequelize from "../config/sequelize";
 
 class User extends Model {};
 
-User.init({});
+User.init({
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
 
 export default User;
