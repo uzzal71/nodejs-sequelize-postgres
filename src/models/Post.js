04330592fb8f78b522bb229a6332f8c31aa6fac5
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-19 00:22:36 +0600

    Post model user_id

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,5 +1,14 @@
+import { DataTypes } from "sequelize";
 import sequelize from "../config/sequelize";
+import User from "./User";
 
-const Post = sequelize.define("Post", {});
+const Post = sequelize.define("Post", {
+    user_id: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    }
+});
+
+User.hasMany();
 
 export default Post;
