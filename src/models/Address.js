e9f69ef0f5a99946eec488714e89b41c1b33d20a
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-29 01:28:18 +0600

    Address model user id added

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,2 +1,13 @@
-import { DataTypes } from "sequelize";
+import { DataTypes } from 'sequelize';
 import sequelize from '../config/sequelize';
+
+const Address = sequelize.define('Address', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: 'users',
+        referencesKay: 'id'
+        
+    }
+});
+
+export default Address;
